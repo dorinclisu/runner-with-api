ba8a23261d13b3e6876c5971c00a61756acd397b
dorinclisu <dorin.clisu@nttdata.com>
2024-11-06 11:51:53 +0200

    Add http_long_polling helper

--- runner_with_api/helpers.py
+++ runner_with_api/helpers.py
@@ -1,11 +1,10 @@
 from collections import deque
-from typing import Deque, Generic, TypeVar
+from typing import AsyncGenerator, Coroutine, Deque, Generic, TypeVar
 import anyio
 
 
 
 T = TypeVar('T')
-
 
 class AnyioDeque(Generic[T]):
     """
@@ -17,17 +16,44 @@ class AnyioDeque(Generic[T]):
 
 
     async def put(self, data: T) -> None:
-        """Does not really block, but needs to be async for the internal synchronization primitives."""
+        """Append to the internal deque. If maxlen is reached, the oldest item is removed.
+        Does not really block, but needs to be async for the internal synchronization primitives."""
         async with self.condition:
             self.deque.append(data)
             self.condition.notify_all()
 
 
     async def get(self) -> T:
-        """Wait for producer if deque is empty."""
+        """Popleft from the internal deque. Wait for producer if deque is empty."""
         async with self.condition:
             while not self.deque:
                 await self.condition.wait()
 
             data = self.deque.popleft()
             return data
+
+
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
+
+async def http_long_polling(func: Coroutine[None, None, T1], keepalive_yield: T2, keepalive_period=1.
+) -> AsyncGenerator[T1 | T2, None]:
+    """Wait for the result of func, while yielding {keepalive_yield} every {keepalive_period} seconds.
+    Once the result is available, yield it and return.
+    """
+    event = anyio.Event()
+
+    async def func_wrapper() -> None:
+        event.result = await func  # type: ignore[attr-defined]
+        event.set()
+
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(func_wrapper)
+
+        while True:
+            with anyio.move_on_after(keepalive_period):
+                await event.wait()
+                yield event.result  # type: ignore[attr-defined]
+                return
+
+            yield keepalive_yield
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,22 +1,40 @@
 
-import anyio
 import pytest
+from anyio import create_task_group, sleep
 
-from runner_with_api.helpers import AnyioDeque
+from runner_with_api.helpers import AnyioDeque, http_long_polling
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_deque():
     deque = AnyioDeque[int](1)
 
     async def put_latest():
         for i in range(10):
             await deque.put(i)
-            await anyio.sleep(0.1)
+            await sleep(0.1)
 
-    async with anyio.create_task_group() as tg:
+    async with create_task_group() as tg:
         tg.start_soon(put_latest)
         assert 0 == await deque.get()
         assert 1 == await deque.get()
-        await anyio.sleep(1)
+        await sleep(1)
         assert 9 == await deque.get()
+
+
+@pytest.mark.anyio
+async def test_long_polling():
+    period = 0.1
+    n = 2
+
+    async def my_task():
+        await sleep(period * (n + 0.5))
+        return 42
+
+    i = 0
+    async for event in http_long_polling(my_task(), keepalive_yield='\n', keepalive_period=period):
+        if i < n:
+            assert event == '\n', i
+        else:
+            assert event == 42, i
+        i += 1
